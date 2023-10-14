3951b73dfa990e422a226c16082c877ffcc67f50
Conner4348 <92555576+Conner4348@users.noreply.github.com>
2023-10-14 10:21:08 -0400

    Started swap_nodes function

--- nodes_and_linked_lists.py
+++ nodes_and_linked_lists.py
@@ -62,23 +62,45 @@ class Linked_List:
                 self.head = None
             else:
                 self.head = current_node.get_next_node()
+            return current_node
         else:
             while next_node:
-                print(f'The value of the current node is {current_node.get_value()}')
+                #print(f'The value of the current node is {current_node.get_value()}')
                 next_node = current_node.get_next_node()
                 if  next_node == None:
-                    continue # continue moves onto the next iteration which will break the loop, since next_node is None.
+                    return None
+                    #continue # continue moves onto the next iteration which will break the loop, since next_node is None.
                 if next_node.get_value() == value:
                     if next_node != None: # Making sure next_node is not None prevents error.
                         current_node.set_link_node(next_node.get_next_node())
                     else:
                         current_node.set_link_node(None)
-                    next_node = None
+                    return next_node
+                    #next_node = None
                 else:
                     current_node = next_node
 
     def swap_nodes(self, value_one, value_two):
-        pass
+        
+        current_node = self.head
+        next_node = 'temp'
+
+        if current_node.get_value() == value_one:
+            node_one = current_node
+            node_one_prev = None
+        if current_node.get_value() == value_two:
+            node_two = current_node
+            node_two_prev = None
+
+        while next_node:
+            next_node = current_node.get_next_node()
+            if next_node == None:
+                pass
+
+# TRY HAVING A LIST THAT CONTAINS BOTH VALUES AND IF A VALUE IS FOUND, REMOVE IT
+# FROM THE LIST.
+# ONCE THE LIST IS EMPTY, THEN SWAP THE NODES WITH THE VALUES.
+                
     
 
 
@@ -92,8 +114,8 @@ db.add_to_end(3)
 db.add_to_beginning(4)
 print(db.stringify_list())
 print('')
-#db.remove_node(3)
+print(db.remove_node(3))
 #db.remove_node(5)
-#db.remove_node(2)
-db.remove_node(4)
+#print(db.remove_node(2))
+#print(db.remove_node(4))
 print(db.stringify_list())
